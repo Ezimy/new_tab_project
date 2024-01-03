03aec4aa1c9bcd7c4f4ac7d6bb1f906afc64e9c9
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-01-03 10:06:36 -0800

    all functionality added

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 
-async function fetchData(){
-    const res = await fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature`, { method: "GET" });
-    const data = await res.json();
-    return data;
+async function fetchBackground(){
+    fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature`, { method: "GET" })
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url('${data.urls.full}')`;
+        document.getElementById("author").innerText = `By: ${data.user.name}`
+    })
 }
-fetchData().then(data => {
-    document.body.style.backgroundImage = `url('${data.urls.full}')`;
-    document.getElementById("author").innerText = `By: ${data.user.name}`
-})
+async function fetchCrypto(){
+    fetch("https://api.coingecko.com/api/v3/coins/ethereum")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML = `
+            <img src = '${data.image.small}'/>
+            <span>${data.name}</span>`
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `
+    })
+    .catch(err => console.error(err))
+}
+function getTime(){
+    const date = new Date()
+    let time = date.toLocaleTimeString("en-us",{timeStyle:"short"})
+    document.getElementById("time").innerText = time
+}
+function getWeather(){
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude
+        const long = position.coords.longitude        
+      fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${lat}&lon=${long}&units=metric`)
+        .then(res => {
+            if(!res.ok){
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+            document.getElementById("weather").innerHTML = `
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
+            <p class="weather-temp">${Math.round(data.main.temp * 10) / 10}°C</p>
+            <p class="location">${data.name}</p>
+            `
+        }).catch(err => console.log(err))
+      });
+}
+fetchBackground()
+fetchCrypto()
+setInterval(getTime, 1000)
+getWeather()
